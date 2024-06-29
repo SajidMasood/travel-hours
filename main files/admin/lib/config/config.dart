56d4449cd54b4d4d5906728dce69cc27bda1190f
sajid <sajid@srptechs.com>
2024-06-29 15:27:36 +0500

    first commit

---
+++ main files/admin/lib/config/config.dart
@@ -0,0 +1,9 @@
+
+class Config{
+  final String appName = 'Travel Hour'; //app name
+  final String testerPassword = 'tester12345';  //testing password
+
+  //firebase server token for push notication
+  final String serverToken = 'AAAA1cgQa-M:APA91bFMD4KswezZfuap6nNBd24JDAIMLXIfni3fH***********';
+  final String icon = 'assets/images/icon.png'; // app icon
+}
